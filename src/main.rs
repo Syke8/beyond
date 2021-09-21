f9c6a93fa65d7ea734a51a33f6322fc42720dbd7
Quentin <qvandiest@gmail.com>
2021-09-21 15:35:46 +0200

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
-use vulkano::{Version, device::physical::PhysicalDevice, instance::{ApplicationInfo, Instance, debug::{DebugCallback, MessageSeverity, MessageType}}};
+use vulkano::{Version, device::{Device, DeviceExtensions, Features, physical::PhysicalDevice}, instance::{ApplicationInfo, Instance, debug::{DebugCallback, MessageSeverity, MessageType}}};
+use vulkano_win::VkSurfaceBuild;
 use winit::{dpi::LogicalSize, event::{KeyboardInput, VirtualKeyCode}, event_loop::{ControlFlow, EventLoop}, window::WindowBuilder};
 
 const WINDOW_WIDTH: u32 = 1920;
 const WINDOW_HEIGHT: u32 = 1080;
-const WINDOW_TITLE: &'static str = "Beyond demo";
+
+const APPLICATION_NAME: &'static str = "Beyond demo";
+const APPLICATION_VERSION: Version = Version { major: 0, minor: 1, patch: 0 };
 
 const ENGINE_NAME: &'static str = "Beyond";
+const ENGINE_VERSION: Version = Version { major: 0, minor: 1, patch: 0 };
 
+// TODO: Starting to be ugly fat code, let's refactor that properly from the  bottom before continuing learning
 fn main() {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title(WINDOW_TITLE)
-        .with_inner_size(LogicalSize::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64))
-        .build(&event_loop);
 
     let app_info = ApplicationInfo {
-        application_name: Some(WINDOW_TITLE.into()),
-        application_version: Some(Version { major: 0, minor: 1, patch: 0 }),
+        application_name: Some(APPLICATION_NAME.into()),
+        application_version: Some(APPLICATION_VERSION),
         engine_name: Some(ENGINE_NAME.into()),
-        engine_version: Some(Version { major: 0, minor: 1, patch: 0 }),
+        engine_version: Some(ENGINE_VERSION),
     };
 
     let mut required_extensions = vulkano_win::required_extensions();
@@ -32,7 +33,32 @@ MessageSeverity { error: true, warning: true, information: true, verbose: true }
         MessageType { general: true, validation: true, performance: true },
     |msg| println!("DEBUG: {}", msg.description)).expect("Cannot create debug callback");
 
-    PhysicalDevice::enumerate(&instance).for_each(|device: PhysicalDevice| println!("{:?}", device));
+    let surface = WindowBuilder::new()
+        .with_title(APPLICATION_NAME)
+        .with_inner_size(LogicalSize::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64))
+        .build_vk_surface(&event_loop, instance.to_owned())
+        .expect("Cannot create Vulkan surface");
+
+    let mut graphics_queue_index = 0;
+
+    let gpu = PhysicalDevice::enumerate(&instance).find(|device| {
+        for (idx, queue) in device.queue_families().enumerate() {
+            if queue.supports_graphics() && surface.is_supported(queue).expect("Surface doesn't support graphics queue") {
+                graphics_queue_index = idx;
+                return true
+            }
+        }
+
+        false
+    }).expect("No available GPU");
+
+    println!("SELECTED : {} - {:?} - {} v{}", gpu.properties().device_name, gpu.properties().device_type, gpu.properties().driver_name.as_ref().unwrap(), gpu.properties().driver_info.as_ref().unwrap());
+
+    let queue_family = gpu.queue_families().nth(graphics_queue_index).unwrap();
+
+    let (device, mut queues) = Device::new(gpu, &Features::none(), &DeviceExtensions::none(), [(queue_family, 1.0), (queue_family, 1.0)]).expect("Cannot create Device");
+    let graphics_queue = queues.next().unwrap();
+    let present_queue = queues.next().unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         match event {
